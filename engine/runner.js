78b88d7b1d436f1a42cc150443599ccaba89a249
NeymarkingingYT <NeymarkingingYT@gmail.com>
2025-08-01 00:32:23 -0700

    Update runner.js

--- engine/runner.js
+++ engine/runner.js
@@ -1,4 +1,7 @@
+// Code For All original blocks btw
+
 Blockly.defineBlocksWithJsonArray([
+  
   {
     "type": "move_forward",
     "message0": "move sprite forward 10px",
@@ -8,9 +11,10 @@ Blockly.defineBlocksWithJsonArray([
     "tooltip": "Moves the sprite forward",
     "helpUrl": ""
   },
+  
   {
     "type": "change_sprite_color",
-    "message0": "change sprite color (not yet implemented)",
+    "message0": "will change sprites color",
     "previousStatement": null,
     "nextStatement": null,
     "colour": 160,
@@ -19,6 +23,8 @@ Blockly.defineBlocksWithJsonArray([
   }
 ]);
 
+// Define Blocks Here
+
 Blockly.JavaScript['move_forward'] = function(block) {
   return 'spriteX += 10; drawStage();\n';
 };
@@ -26,6 +32,13 @@ Blockly.JavaScript['move_forward'] = function(block) {
 Blockly.JavaScript['change_sprite_color'] = function(block) {
   return '// Change sprite color code goes here\n';
 };
+
+Blockly.JavaScript['say_hello'] = function(block) {
+  const msg = block.getFieldValue('TEXT');
+  return `alert("${msg}");\n`;
+};
+
+// extra blocks code
 
 Blockly.defineBlocksWithJsonArray([
   {
@@ -45,3 +58,11 @@ Blockly.defineBlocksWithJsonArray([
     "helpUrl": ""
   }
 ]);
+
+// block definer
+
+Blockly.JavaScript['say_hello'] = function(block) {
+  const msg = block.getFieldValue('TEXT');
+  return `alert("${msg}");\n`;
+};
+
